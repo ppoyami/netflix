bed6431eb0ac3a4c898f84d5b0955ee3f67d1278
scvd03 <scvd03@gmail.com>
2021-12-19 15:45:46 +0900

    ✨  Add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,22 @@ import { Global, ThemeProvider } from '@emotion/react';
 
 import GlobalStyles from '@styles/global';
 import theme from '@styles/theme';
+
+import Header from '@components/Header';
 import Home from '@pages/Home';
+import Tv from '@pages/Tv';
+import Search from '@pages/Search';
 
 export default function App() {
   return (
     <div>
       <ThemeProvider theme={theme}>
         <Global styles={GlobalStyles(theme)} />
+        <Header />
         <Switch>
           <Route path="/" component={Home} />
+          <Route exact path="/tv" component={Tv} />
+          <Route exact path="/search" component={Search} />
         </Switch>
       </ThemeProvider>
     </div>
---
+++ src/pages/Tv.tsx
@@ -0,0 +1,7 @@
+export default function Tv() {
+  return (
+    <div>
+      <h1>Tv</h1>
+    </div>
+  );
+}

615f7ee2366024e464b1baa2c692d7095e357133
scvd03 <scvd03@gmail.com>
2021-12-19 17:45:06 +0900

    🏷️  Definition Types (Movie)

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,25 @@
 const API_KEY = 'b155ee0fa93810739398bb8ba9675654';
 const BASE_PATH = 'https://api.themoviedb.org/3';
 
+interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
 export function getMovies() {
   return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
     response => response.json(),

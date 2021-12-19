01cf8a6b6e1dd6b6926045e81210d08df20e5e60
scvd03 <scvd03@gmail.com>
2021-12-19 17:19:07 +0900

    ✨ New: Fetch Data

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import App from '@app';
 
+const client = new QueryClient();
+
 ReactDOM.render(
   <Router>
-    <App />
+    <QueryClientProvider client={client}>
+      <App />
+    </QueryClientProvider>
   </Router>,
   document.getElementById('root'),
 );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,18 @@
+import { useQuery } from 'react-query';
 import styled from '@emotion/styled';
 
+import { getMovies } from '../api';
+
 export default function Home() {
+  const { data, isLoading } = useQuery(['movies', 'nowPlaying'], getMovies);
+  console.log(data, isLoading);
   return (
     <Layout>
-      <h1>Hello, World!</h1>
+      <h1></h1>
     </Layout>
   );
 }
 
-const Layout = styled.div({
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-});
+const Layout = styled.div`
+  height: 200vh;
+`;

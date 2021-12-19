ffb601a5da49919cbd8c24881e1ee39a0caa0049
scvd03 <scvd03@gmail.com>
2021-12-19 18:34:03 +0900

    ✨ New: Slider

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,15 +8,17 @@ import { makeImagePath } from '../utils';
 
 const rowVariants = {
   hidden: {
-    x: window.outerWidth + 10,
+    x: window.outerWidth + 5,
   },
   visible: {
     x: 0,
   },
   exit: {
-    x: -window.outerWidth - 10,
+    x: -window.outerWidth - 5,
   },
 };
+
+const offset = 6;
 
 export default function Home() {
   const { data, isLoading } = useQuery<IGetMoviesResult>(
@@ -24,8 +26,18 @@ export default function Home() {
     getMovies,
   );
   const [index, setIndex] = useState(0);
-  const incraseIndex = () => setIndex(prev => prev + 1);
-
+  const [leaving, setLeaving] = useState(false);
+  const incraseIndex = () => {
+    if (data) {
+      //M: index가 연속으로 두번 증가하는 것을 막아줍니다.
+      if (leaving) return;
+      toggleLeaving(); // * index를 증가할 때, leaving을 true로 세팅
+      const totalMovies = data.results.length - 1;
+      const maxIndex = Math.floor(totalMovies / offset) - 1;
+      setIndex(prev => (prev === maxIndex ? 0 : prev + 1));
+    }
+  };
+  const toggleLeaving = () => setLeaving(prev => !prev);
   return (
     <Layout>
       {isLoading ? (
@@ -40,7 +52,8 @@ export default function Home() {
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
           <Slider>
-            <AnimatePresence>
+            {/* row의 이동이 완전히 끝났을 때, leaving을 false로 하여 인덱스가 증가될 수 있도록 합니다. */}
+            <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
               <Row
                 variants={rowVariants}
                 initial="hidden"
@@ -49,9 +62,15 @@ export default function Home() {
                 transition={{ type: 'tween', duration: 1 }}
                 key={index}
               >
-                {[1, 2, 3, 4, 5, 6].map(i => (
-                  <Box key={i}>{i}</Box>
-                ))}
+                {data?.results
+                  .slice(1)
+                  .slice(offset * index, offset * index + offset)
+                  .map(movie => (
+                    <Box
+                      key={movie.id}
+                      bgPhoto={makeImagePath(movie.backdrop_path, 'w500')}
+                    />
+                  ))}
               </Row>
             </AnimatePresence>
           </Slider>
@@ -61,7 +80,9 @@ export default function Home() {
   );
 }
 
-const Layout = styled.div``;
+const Layout = styled.div`
+  padding-bottom: 200px;
+`;
 
 const Loader = styled.div`
   height: 20vh;
@@ -98,15 +119,17 @@ const Slider = styled.div`
 
 const Row = styled(motion.div)`
   display: grid;
-  gap: 10px;
+  gap: 5px;
   grid-template-columns: repeat(6, 1fr);
   position: absolute;
   width: 100%;
 `;
 
-const Box = styled(motion.div)`
+const Box = styled(motion.div)<{ bgPhoto: string }>`
   background-color: white;
   height: 200px;
-  color: red;
+  background-image: url(${props => props.bgPhoto});
+  background-size: cover;
+  background-position: center center;
   font-size: 66px;
 `;
